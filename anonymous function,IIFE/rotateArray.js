c80f2cf10c965faffef256014c20de91d0b2e01b
Akhilesh C <akhileshcvdy@gmail.com>
2023-12-26 12:20:58 +0530

    very final commit

--- anonymous function,IIFE/rotateArray.js
+++ anonymous function,IIFE/rotateArray.js
@@ -1,6 +1,7 @@
 var array = [1, 3, 4, 5, 6];
 var times = 2;
 
+//anonymous
 const rotateArray = function (arr, k) {
   var temp;
   for (let i = 0; i < k; i++) {
@@ -11,3 +12,13 @@ const rotateArray = function (arr, k) {
 };
 
 console.log(rotateArray(array, times));
+
+//IIFE
+(function (arr, k) {
+  var temp;
+  for (let i = 0; i < k; i++) {
+    temp = arr.pop();
+    arr.unshift(temp);
+  }
+  console.log(arr);
+})(array, times);
